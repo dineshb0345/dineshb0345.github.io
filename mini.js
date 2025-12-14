50c69314d1e89d1bc077236e81d80478740b1ef3
dineshb0345 <dineshbaskar0307@gmail.com>
2025-12-14 21:41:30 +0530

    Delete mini.js

--- mini.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out! I will get back to you soon.');
-});
